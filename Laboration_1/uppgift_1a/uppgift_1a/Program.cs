387f84b61bad4d8e2685d55fb93b5696bd7227d8
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-11-13 20:06:58 +0100

    Assigment 1a complete

--- Laboration_1/uppgift_1a/uppgift_1a/Program.cs
+++ Laboration_1/uppgift_1a/uppgift_1a/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uppgift_1a
+namespace RaknaA
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string RaknaAString;
+            int storaA;
+            int smaa;
+
+            RaknaAString = Console.ReadLine();
+
+            storaA = Uppercase(RaknaAString);
+            smaa = Lowercase(RaknaAString);
+
+            Console.WriteLine("Antal a: " + smaa);
+            Console.WriteLine("Antal A: " + storaA);
         }
+
+        #region Methods
+        private static int Uppercase(string RaknaA)
+        {
+            int oldString = RaknaA.Length;
+            int newString = oldString - RaknaA.Replace("A", "").Length;
+
+            return newString;
+        }
+
+        private static int Lowercase(string RaknaA)
+        {
+            int oldString = RaknaA.Length;
+            int newString = oldString - RaknaA.Replace("a", "").Length;
+
+            return newString;
+        }
+        #endregion
     }
 }
