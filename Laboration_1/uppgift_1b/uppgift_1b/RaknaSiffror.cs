13e87ce872f2b26e011069ff5d708efaa84ac8dd
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-11-15 20:05:26 +0100

    Class created and methods and properties. Error handling on input implemented.

---
+++ Laboration_1/uppgift_1b/uppgift_1b/RaknaSiffror.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uppgift_1b
+{
+    public class RaknaSiffror
+    {
+        public RaknaSiffror()
+        {
+            Input = ReadInput();
+        }
+
+        public int Zero { get; set; }
+
+        public int Odd { get; set; }
+
+        public int Even { get; set; }
+
+        public int Input { get; set; }
+    
+        public int NumberOfZero()
+        {
+            return Input;
+        }
+
+        public int NumberOfOdd()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int NumberOfEven()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ReadInput()
+        {
+            int number;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Mata in ett heltal: ");
+                    number = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Felaktig inmatning!");
+                }
+                
+            }
+            return number;
+        }
+    }
+}
