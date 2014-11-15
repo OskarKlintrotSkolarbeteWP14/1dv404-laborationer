94c8235711c091cb9812c5cd4a0713c69906a02b
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-11-15 20:38:28 +0100

    Cleaning up in the code.

--- Laboration_1/uppgift_1b/uppgift_1b/Program.cs
+++ Laboration_1/uppgift_1b/uppgift_1b/Program.cs
@@ -10,11 +10,6 @@ namespace uppgift_1b
     {
         static void Main(string[] args)
         {
-            int heltal = -1;
-            
-            
-            
-
             RaknaSiffror Rakna = new RaknaSiffror();
 
             Console.WriteLine(Rakna.NumberOfZero());
