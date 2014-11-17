87b77041b9c09bdac5c67949e22e473d275d300d
Oskar Klintrot <oskar.klintrot@gmail.com>
2014-11-17 10:40:22 +0100

    Input to int works with error handling

--- Laboration_1/uppgift_1c/uppgift_1c/Program.cs
+++ Laboration_1/uppgift_1c/uppgift_1c/Program.cs
@@ -10,6 +10,28 @@ namespace uppgift_1c
     {
         static void Main(string[] args)
         {
+            int test = ReadInput();
+            Console.WriteLine(test);
+        }
+
+        private static int ReadInput()
+        {
+            string stringNumber;
+            int intNumber;
+
+            Console.WriteLine("Mata in ett heltal: ");
+            while (true)
+            {
+                stringNumber = Console.ReadLine();
+                if (!(int.TryParse(stringNumber, out intNumber)))
+                {
+                    Console.WriteLine("Felaktig inmatning, försök igen: ");
+                    continue;
+                }
+                break;
+            }
+
+            return intNumber;
         }
     }
 }
